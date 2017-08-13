48206b9c4fc536a94a4ceb77268eb8c72b777e4f
Enner Pérez <ennerperez@gmail.com>
2017-08-13 00:23:20 -0400

    Version fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
-## [1.1.16] - [2017-04-20]
+## [1.2.20] - [UNRELEASED]
 ### Added
 - Xamarin Forms Implementation
 
--- src/Pictograms/Properties/AssemblyInfo.Common.cs
+++ src/Pictograms/Properties/AssemblyInfo.Common.cs
@@ -22,6 +22,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
