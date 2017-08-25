32a8d400d99364c661fec2f44da01a3d1e1c0b15
Enner Pérez <ennerperez@gmail.com>
2017-08-24 23:28:05 -0400

    AssemblyInfo Fix

--- src/Pictograms.Forms/Properties/AssemblyInfo.cs
+++ src/Pictograms.Forms/Properties/AssemblyInfo.cs
@@ -8,10 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Pictograms Forms")]
 [assembly: AssemblyProduct("Pictograms Forms")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("45872AA9-7722-49B4-864D-B03F11AB5B3F")]
--- src/Pictograms/Properties/AssemblyInfo.cs
+++ src/Pictograms/Properties/AssemblyInfo.cs
@@ -7,10 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Pictograms")]
 [assembly: AssemblyProduct("Pictograms")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0D9E79A0-DBCF-42A6-B0A8-6234F3AAE253")]
